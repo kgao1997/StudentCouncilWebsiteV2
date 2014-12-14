59215c4dd1c51dd3f7226e44e64de803935d30fd
kgao1997 <kgao1997@gmail.com>
2014-12-14 17:43:56 -0500

    added classes

--- app/views/aboutUs.blade.php
+++ app/views/aboutUs.blade.php
@@ -4,12 +4,12 @@
 
 <br>
 Officers<br>
-President:  Angela Hourihan<br>
-Vice-President:  Gloribel Rivas<br>
-Secretary:  Christina Yee  <br>
-Treasurer:  Ana Battaglino  <br>
-External Liasions:  Haben Kelati and Randy Chen  <br>
-IT Director:  Kevin Gao<br>
+President:  Angela Hourihan (Class II)<br>
+Vice-President:  Gloribel Rivas (Class I)<br>
+Secretary:  Christina Yee (Class III)<br>
+Treasurer:  Ana Battaglino  (Class II)<br>
+External Liasions:  Haben Kelati (Class I) and Randy Chen (Class II)  <br>
+IT Director:  Kevin Gao (Class I) <br>
 <br>
 If you have questions, concerns, or just simply want to get involved in the Student Council, please email  blsstudentcouncil@gmail.com.  We'll add you to our mailing list and we promise to get back to you ASAP<br>
 <br>
