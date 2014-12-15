83b3fb60293022477901fc68e439dd567cfdc156
kgao1997 <kgao1997@gmail.com>
2014-12-15 18:25:15 -0500

    updated with Survey

--- app/views/aboutUs.blade.php
+++ app/views/aboutUs.blade.php
@@ -7,8 +7,8 @@ Officers<br>
 President:  Angela Hourihan (Class II)<br>
 Vice-President:  Gloribel Rivas (Class I)<br>
 Secretary:  Christina Yee (Class III)<br>
-Treasurer:  Ana Battaglino  (Class II)<br>
-External Liasions:  Haben Kelati (Class I) and Randy Chen (Class II)  <br>
+Treasurer:  Ana Battaglino  (Class III)<br>
+External Liasions:  Haben Kelati (Class I) and Randy Chen (Class III)  <br>
 IT Director:  Kevin Gao (Class I) <br>
 <br>
 If you have questions, concerns, or just simply want to get involved in the Student Council, please email  blsstudentcouncil@gmail.com.  We'll add you to our mailing list and we promise to get back to you ASAP<br>
--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -19,13 +19,23 @@ and to represent them and their needs in our relationship with the school admini
 We seek to train and install a network of leaders that work together,<br>
 gaining leadership abilities and skills while sponsoring events <br>
 and working to solve the problems that face their fellow classmates. <br>
+<br>
+<br> 
+
+Here's a survey for your opinions!<br>
+Please fill it out if you have the time.  All answers are anonymous<br>
+Thank You!<br>
+<br>
+<a href = "https://docs.google.com/forms/d/1qlVvBQY0weXTmwQ-89R2S0mR563PWun3SjUBLZJMwaY/viewform"> Student Opinion Survey </a>
+
+
 </div>
 
 <div id = "Twitter Feed"; style = 
 " float: right;
   margin-right: 50px;
 ";>
-<a class="twitter-timeline" width="300"; height="500"; href="https://twitter.com/BLSStuCo" data-widget-id="532366160193269760">Tweets by @BLSStuCo</a>
+<a class="twitter-timeline" width="300"; height="700"; href="https://twitter.com/BLSStuCo" data-widget-id="532366160193269760">Tweets by @BLSStuCo</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
 
