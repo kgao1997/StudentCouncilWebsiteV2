c785e707079a139817cfef610293b070e2abd9f6
kgao1997 <kgao1997@gmail.com>
2015-01-08 18:33:24 -0500

    added more breaks

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -18,10 +18,10 @@ We need your opinions! Please complete this brief, anonymous survey. Thank You!<
 <h1>Mission Statement</h1><br>
 We seek to provide an open forum for students concerns and ideas for improving Boston Latin School<br>
 in order to make it a more effective and enjoyable learning environment for all.<br>
-<br>
+<br><br>
 We seek to be the voice of the students of Boston Latin School<br>
 and to represent them and their needs in our relationship with the school administration.<br>
-<br>
+<br><br>
 We seek to train and install a network of leaders that work together,<br>
 gaining leadership abilities and skills while sponsoring events <br>
 and working to solve the problems that face their fellow classmates. <br>
