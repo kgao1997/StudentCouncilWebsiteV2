f70fa5d0c26e668c1e466bcfb2c1c5da28362119
kgao1997 <kgao1997@gmail.com>
2015-01-10 00:01:59 -0500

    updated 1/10

--- app/views/events.blade.php
+++ app/views/events.blade.php
@@ -15,8 +15,23 @@ Open House- November 3rd<br>
 The Open House is an opportunity for all members of the BLS community to look into joining the Student Council,<br>
 find out what we do, or just voice their thoughts.  Come join us!<br>
 <br>  
-Globe Santa Drive-  Will be in December<br>
+Globe Santa Drive-  December <br>
+<br>
+January 12th- Member Appreciation Day! <br>
+Come in and enjoy snacks and good times as we have a small party after school and share the results of the community <br>
+forum survey!<br>
+<br>
+Open Community Forum- January 20th <br>
+We will be discussing the issues outlined in the recent Student Climate Survey and how they affect both our school<br>
+community and our local communities.  <br>
+Come one come all! <br>
+<br>
+Guest Speaker - January 26th<br>
+We will be having a special guest speaker come in and share her experiences on January 26th.<br>
+Check back for more details!<br>
+<br>
 Student-Faculty Basketball Game- Will be in March-April<br>
+<br>
 Crystal Apple Awards- June<br>
 <br>
 Events TBD:  <br>
--- app/views/news.blade.php
+++ app/views/news.blade.php
@@ -2,9 +2,6 @@
 
 @section('content')
 <div align="center">
-There will be no Student Council Meeting tomorrow (11/10/2014)<br>
-<br>
-<br>
-The next meeting will be Monday, 11/17/2014
+Check back for the most recent news!
 
 @stop
