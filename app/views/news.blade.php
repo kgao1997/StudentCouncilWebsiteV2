6eba04ef801987435e8aeab55784fb2d8bf91aad
kgao1997 <kgao1997@gmail.com>
2015-01-19 23:59:45 -0500

    fixed date

--- app/views/news.blade.php
+++ app/views/news.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div align="center">
-Due to scheduling constraints, the Guest Speaker has been moved to Febuary 6th.  <br>
+Due to scheduling constraints, the Guest Speaker has been moved to Febuary 9th.  <br>
 We hope to see you there!<br>
 <br>
 Join the Open Community Forum on January 20th!
